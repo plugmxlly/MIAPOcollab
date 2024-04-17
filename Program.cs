48c90ba7ce4e5bae0cde226708f19420b9819f6b
Белодед Евгений Олегович <gr621_beeol@ttit.local>
2024-04-17 18:33:58 +0700

    first global commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Calc
+    {
+        static void Main()
+        {
+            
+            switch (oper) {
+                case '+':
+                    Plus(num1, num2);
+                    break;
+
+                case '-':
+                    Minus(num1, num2);
+                    break;
+
+                case '*':
+                    Multiply(num1, num2);
+                    break;
+
+                case '/':
+                    Divide(num1, num2);
+                    break;
+
+                default:
+                    Console.WriteLine("Неизвестный оператор");
+                    break;
+
+            }
+        }
+    }
+}
